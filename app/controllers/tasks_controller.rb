5f313044742c75cdb0eb520c77f4807a36958a2a
banelengwane <banelengwane@gmail.com>
2023-08-30 14:28:01 +0200

    Making new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,22 @@
 class TasksController < ApplicationController
   def index
-    @tasks = []
+    #how to return all tasks in the database
+    @tasks = Task.all
   end
 
+  #this is where create a new task
   def new
+    @task = Task.new
+  end
+
+  def create 
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
