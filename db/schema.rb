b0770595b119fe9ed94f1412edb0b49d5ed19a28
banelengwane <banelengwane@gmail.com>
2023-09-06 15:09:29 +0200

    Making migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_140542) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_130243) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
