515c10eda6007dae6770b0938a82d6d2d21d76ed
banelengwane <banelengwane@gmail.com>
2023-10-10 13:26:09 +0200

    session controller routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
   get 'welcome/index'
   root 'welcome#index'
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'log_in', to: 'sessions#new'
+  get 'log_out', to: 'sessions#destroy'
+
+  resources :users
+  get 'signup', to: 'users#new'
   resources :tasks
 
   get 'tasks/:id/complete/:completed', to: 'tasks#complete'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_142346) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_100021) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_142346) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
