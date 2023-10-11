30629a2cfe10e62cc6c943a21c0f9f0519c8b944
banelengwane <banelengwane@gmail.com>
2023-10-11 15:17:53 +0200

    Errors and Flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to :tasks 
+      redirect_to :tasks, notice: "Login successful"
     else 
+      flash.alert = "Login failed."
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to root_path, notice: "Logged Out. goodby"
   end
 end
