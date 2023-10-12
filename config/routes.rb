0d08366d04c619686bbb0bfa4d83deeb300a9d78
banelengwane <banelengwane@gmail.com>
2023-10-12 12:12:48 +0200

    Clean up, unused functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,14 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
   get 'sessions', to: 'sessions#new'
-  resources :users
+
+  resources :users, only: [:new, :create]
   get 'signup', to: 'users#new'
+
   resources :tasks
-
   get 'tasks/:id/complete/:completed', to: 'tasks#complete'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 end
