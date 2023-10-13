57272b9ab5acdd826fcf84d145ad4dd8316640f3
banelengwane <banelengwane@gmail.com>
2023-10-13 10:42:41 +0200

    added rspec for TDD

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "Logged Out. goodby"
+    redirect_to root_path, notice: "Logged Out. goodbye"
   end
 end
